1ed23bcb1d0cdb383c762d34c5475bd1c500588d
jli27 <96714100+jli27@users.noreply.github.com>
2025-01-24 19:56:20 +0000

    finished

--- dice.rb
+++ dice.rb
@@ -11,24 +11,34 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "Hello World"
+  erb(:elephant)
 end
 
-get("/zebra") do
-  "We must add a route for each path we want to support"
-end
+#get("/zebra") do
+#  "We must add a route for each path we want to support"
+#end
 
-get("/giraffe") do
-  "Hopefully this shows up without having to restart the server 🤞🏾"
-end
+#get("/giraffe") do
+#  "Hopefully this shows up without having to restart the server 🤞🏾"
+#end
 
 get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:two_six, { :layout => :wrapper })
+end
+
+get ("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..19)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  
+  "<h1>2d10</h1>
+  <p>#{outcome}</p>"
 end
